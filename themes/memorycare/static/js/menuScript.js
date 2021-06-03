b26475a23c76c253f34bf890067e887b8b0e089b
Andrew M McCall <andrew.m.mccall@gmail.com>
2021-06-03 17:13:19 -0400

    Menu Toggle Function

--- themes/memorycare/static/js/menuScript.js
+++ themes/memorycare/static/js/menuScript.js
@@ -1,10 +1,19 @@
 const menuToggle = document.querySelectorAll('.subToggle > a');
 
+function slideToggle(el) {
+    el.style.height = el.scrollHeight + 'px';
+    el.scrollHeight = el.scrollHeight;
+
+    el.classList.toggle('active');
+    el.style.height = el.classList.contains('active') ? el.scrollHeight + 'px' : 0;
+}
+
+
+
 menuToggle.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         const submenu = e.target.parentNode.parentNode.parentNode.children[3];
-        const expanded = submenu.getBoundingClientRect().height;
-        submenu.style.height = 0;
-        submenu.style.height = expanded + 'px';
+        slideToggle(submenu);
+
     })
 })
