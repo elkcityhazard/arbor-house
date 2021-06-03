78c8240ffcc12e7b726682863f757759c8e3db41
Andrew M McCall <andrew.m.mccall@gmail.com>
2021-06-02 21:17:37 -0400

    single page template

--- content/about/index.md
+++ content/about/index.md
@@ -1,5 +1,25 @@
 ---
-title: About Arbor House Traverse City
+title: "About Us"
+subtitle: "Arbor House nursing home specializes in Alzheimer's care and Dementia care in Traverse City, Michigan."
 layout: single
+singleCSS: true
+imageURL: /images/cover-photo-3.jpg
+figcaption: "Our beautiful custom built home is close to Traverse City, yet nestled alongside the Pelizzari Natural Area on a quiet wooded cul de sac on Old Mission Peninsula."
 ---
-# Hello World
+
+Arbor House Assisted Living home (ALH) specializes in providing total care for seniors with memory loss, alzheimer’s care and dementia care. The beautiful custom built home is close to Traverse City, yet nestled alongside the [Pelizzari Natural Area](https://www.gtrlc.org) on a quiet wooded cul de sac on Old Mission Peninsula. The peaceful setting allows the residents to regain their sense of independence by enjoying the lush grounds and walkways which are fenced and secured.
+
+Arbor House Assisted Living Home boasts large rooms for the residents to gather, socialize, watch TV , read, etc. . The family-like atmosphere stimulates participation through daily activities for example folding towels, setting the table, arranging flowers from the garden or making Peach cake. Several nooks, a sun room with fireplace, front porch and back deck provide private areas for reading or a nap.
+
+The Arbor House (ALH) is owned by Cindy Robertson, native of Traverse City. A graduate of MSU, Cindy has over 20 years experience in social work and health care. Cindy is an active member of Michigan Assisted Living Association, BASA with extensive training in Alzheimer’s care, dementia care and memory loss care. Her team of professional caregivers provide highly personalized care for the residents at the state licensed assisted living home. Home-cooked balanced meals, baked goodies, clean facilities and excellent healthcare are provided round the clock. After serving families of the Grand Traverse area for 6 years, Arbor House (ALH) has earned a respected reputation in the community.
+
+Available in- home services include a physical therapist, beautician, foot care, exercise, arts, crafts, pet therapy and music therapy for dementia. Cindy escorts the residents to medical appointments, shopping or a scenic drive along the bay. Planned activities include themed holiday decorations, a holiday party for residents and their guests, birthday celebrations and the annual group outing to the Alpaca farm.
+
+Arbor House (ALH) staff is trained to provide long term care as medical/health needs arise, allowing the resident to stay with the same caregivers in the same setting they are familiar with. Working together with Hospice, Arbor House ensures the resident dignity and grace and comfort in their end of life care.
+
+Cindy and her team extend a warm invitation to visit Arbor House Assisted Living Home. Please call Cindy to arrange a tour.
+
+Arbor House Assisted living  
+[Michigan Assisted Living Assoc. – MALA](https://www.miassistedliving.org)    
+[Bay Area Senior Advocates – BASA](http://basatc.org/business-directory/arbor-house-asstd-living/)
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,75 +1,107 @@
 (function () {
-const navToggle = document.querySelector('.hamburger-container');
-const hamburger = document.querySelector('.hamburger-container > span')
-const navBar = document.querySelector('.navbar-main > nav');
-navToggle.addEventListener('click', (e) => {
-    hamburger.classList.toggle('active')
-    navBar.classList.toggle('active');
-})})()
-
-
-
+  const navToggle = document.querySelector(".hamburger-container");
+  const hamburger = document.querySelector(".hamburger-container > span");
+  const navBar = document.querySelector(".navbar-main > nav");
+  navToggle.addEventListener("click", (e) => {
+    hamburger.classList.toggle("active");
+    navBar.classList.toggle("active");
+  });
+})();
 
 // Slider
 
-const slideContainer = document.querySelector('.carousel-inner');
-const slides = document.querySelectorAll('.carousel-item');
-let counter = 1;
+// const slideContainer = document.querySelector(".carousel-inner");
+// const slides = document.querySelectorAll(".carousel-item");
+// let counter = 1;
 
-function changeSlide() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    });
+// function changeSlide() {
+//   slides.forEach((slide) => {
+//     slide.classList.remove("active");
+//   });
 
-    if (counter > slides.length - 1) {
-        counter = 0;
-    }
-    if (counter < 0) {
-        counter = 0;
-    }
-    slides[counter].classList.add('active')
-slideContainer.style.transform = `translateX(${counter * -100}%)`;
-counter += 1;
-console.log(counter)
-}
+//   if (counter > slides.length - 1) {
+//     counter = 0;
+//   }
+//   if (counter < 0) {
+//     counter = 0;
+//   }
+//   slides[counter].classList.add("active");
+//   slideContainer.style.transform = `translateX(${counter * -100}%)`;
+//   counter += 1;
+//   console.log(counter);
+// }
 
-let timer = setInterval(changeSlide, 7000);
+// let timer = setInterval(changeSlide, 2000);
 
-slideContainer.addEventListener('click', () => {
-    clearInterval(timer);
-})
-
-
+// slideContainer.addEventListener("click", () => {
+//   clearInterval(timer);
+// });
 
 //lazy load images
 
-const images = document.querySelectorAll('.lazy');
+const images = document.querySelectorAll(".lazy");
 
 const config = {
-    rootMargin: '15px 0px 0px 15px',
-  threshold: [1, 0.5, 0.5, 1]
+  rootMargin: "15px 0px 0px 15px",
+  threshold: [1, 0.5, 0.5, 1],
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0) {
+      console.log();
+      entry.target.src = entry.target.getAttribute("data-src");
+      entry.target.classList.add("fadeIn");
+      observer.unobserve(entry.target);
+    }
+  });
+}, config);
+
+images.forEach((image) => {
+  observer.observe(image);
+});
+
+function parallax(el, multiplier) {
+  window.addEventListener("scroll", () => {
+    const cont = document.querySelector(el);
+    const y = document.documentElement.getBoundingClientRect().top;
+    const x = document.documentElement.scrollWidth;
+    const rate = y / x * multiplier;
+    console.log(x, y, rate)
+    if (y < 0) {
+      cont.style.backgroundPosition = `center ${rate}px`;
+    }
+  });
 }
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-            entry.target.src = entry.target.getAttribute('data-src');
-            entry.target.classList.add('fadeIn');
-            observer.unobserve(entry.target);
-        }
-    })
-}, config)
-
-images.forEach(image => {
-    observer.observe(image);
+window.addEventListener('load', () => {
+  const services = document.querySelectorAll('.service');
+  const siteHeader = document.getElementById('siteTitle');
+  const siteSubtitle = document.getElementById('siteSubtitle');
+  const ctaBtn = document.querySelector('header.header-main .cta-btn');
+  siteHeader.style.opacity = 1;
+  siteHeader.style.transform = "translateY(0)";
+  siteSubtitle.style.opacity = 1;
+  siteSubtitle.style.transform = "translateY(0)";
+  ctaBtn.style.opacity = 1;
+  for (let i = 0; i < services.length; i++) {
+    setTimeout(() => {
+      services[i].style.opacity = 1;
+      services[i].style.transform = "translateX(0)";
+    }, 1500 * i)
+  }
 })
+  
 
 
-const yearContainer = document.getElementById('currentYear');
+
+
+
+
+
+const yearContainer = document.getElementById("currentYear");
 const currentYear = new Date().getFullYear();
 
 yearContainer.textContent = currentYear;
 yearContainer.style.textDecoration = "none";
 yearContainer.style.display = "inline";
-
-
