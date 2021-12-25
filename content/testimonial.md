8dcc75ab65a3f8ec61b486574eff6a2b8700bd80
Andrew M McCall <andrew.m.mccall@gmail.com>
2021-12-25 12:51:43 -0500

    testimonial slider update

--- content/testimonial.md
+++ content/testimonial.md
@@ -3,22 +3,26 @@ title: Testimonials
 headless: true
 reviews: 
     - name: Assenmacher Family
+      id: 0
       image: "/images/reviews/review-1.png"
       stars: 5
       text: "**Dear Arbor House Family**, We cannot express enough thanks for your care and kindness of our dad at the end of his life.  Some of his last words were \"I would not have wanted to die any other way.\" Your warmth and kindness brought him peace and comfort in his final days.  May the Lord bless you all 10-fold of the compassion and mercy you showed to Bob, and all your charges.  With sincerest affection, the **Assenmacher Family**"  
       date: 2021-12-19
     - name: Louise Kelly & Family
-      image: "/images/quality-of-life-icon.jpg"
+      id: 1
+      image: "/images/reviews/review-place-holder-300x300.webp"
       stars: 5
       text: "Our thanks to you all for the excellent care you gave to George. I’m sorry he had to endure Bortz, and i know they gave him as much care as they could, before Tom made the transfer to Arbor House. As I’m sure you were aware, I would have loved having him home if it were possible. The family were impressed with the loving care you gave their dad. We appreciated the card you sent and it will be kept with so many others in a special folder."  
       date: 2021-06-15
     - name: Evelyn and Richard Spencer
-      image: "/images/quality-of-life-icon.jpg"
+      id: 2
+      image: "/images/reviews/review-place-holder-300x300.webp"
       stars: 5
       text: "Thank you (and your staff) for taking such good care of our dad, Clyde Spencer. We had a wonderful fishing trip knowing that dad was well cared for. Also, thank you for helping our daughter take dad to the doctor. You are very caring and professional. Thank you for the note, one of these days I will stop to see you. Grateful for your care of George."  
       date: 2021-06-15
     - name: Holly Cooper
-      image: "/images/quality-of-life-icon.jpg"
+      id: 3
+      image: "/images/reviews/review-place-holder-300x300.webp"
       stars: 5
       text: "I completely and totally recommend Arbor House. They provide a high level of care, support and supervision in a home-like environment. The staff cook the meals on the premises and make special dishes to accommodate the needs and sometimes finicky appetites of the elderly residents. They are very aware of the importance of getting the residents out of their beds and rooms and bringing them to the common areas to socialize."  
       date: 2021-06-15
--- public/js/script.js
+++ public/js/script.js
@@ -2,35 +2,26 @@ const parallax = (id, rate) => {
   let el = document.querySelector(id);
 
   const sp = () => {
-      //   Define X and Y
+    //   Define X and Y
     let x = el.getBoundingClientRect().top / rate;
     let y = Math.round(x * 100) / 100;
 
-    //   Define Background Position Dynamically 
+    //   Define Background Position Dynamically
 
-    el.style.backgroundPosition = 'center ' + y + 'px';
-
-  }
+    el.style.backgroundPosition = "center " + y + "px";
+  };
 
   // initiate parameter function to position background;
   sp();
 
   // initiate scroll event listener and call parameter callback on scroll event
 
-  window.addEventListener('scroll', function() { sp() });
+  window.addEventListener("scroll", function () {
+    sp();
+  });
+};
 
-}
-
-parallax('.fixed-background', 3);
-
-
-
-
-
-  
-
-
-
+parallax(".fixed-background", 3);
 
 
 
@@ -44,34 +35,6 @@ parallax('.fixed-background', 3);
   });
 })();
 
-// Slider
-
-// const slideContainer = document.querySelector(".carousel-inner");
-// const slides = document.querySelectorAll(".carousel-item");
-// let counter = 1;
-
-// function changeSlide() {
-//   slides.forEach((slide) => {
-//     slide.classList.remove("active");
-//   });
-
-//   if (counter > slides.length - 1) {
-//     counter = 0;
-//   }
-//   if (counter < 0) {
-//     counter = 0;
-//   }
-//   slides[counter].classList.add("active");
-//   slideContainer.style.transform = `translateX(${counter * -100}%)`;
-//   counter += 1;
-//   console.log(counter);
-// }
-
-// let timer = setInterval(changeSlide, 2000);
-
-// slideContainer.addEventListener("click", () => {
-//   clearInterval(timer);
-// });
 
 //lazy load images
 
@@ -81,36 +44,34 @@ function lazyLoad(elements) {
   //  Config for Intersection Observer
   const config = {
     rootMargin: "15px 0px 0px 15px",
-    threshold: [1, 0.5, 0.5, 1]
+    threshold: [1, 0.5, 0.5, 1],
   };
 
   // Create Observer
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.intersectionRatio > 0) {
-        entry.target.src = entry.target.getAttribute('data-src');
-        entry.target.classList.add('fadeIn');
+        entry.target.src = entry.target.getAttribute("data-src");
+        entry.target.classList.add("fadeIn");
         observer.unobserve(entry.target);
       }
-    })
+    });
   }, config);
 
   elements.forEach((element) => {
     observer.observe(element);
-  })
-
-
+  });
 }
 
 const images = document.querySelectorAll(".lazy");
 
 const config = {
   rootMargin: "15px 0px 0px 15px",
-  threshold: [1, 0.5, 0.5, 1]
+  threshold: [1, 0.5, 0.5, 1],
 };
 
-const observer = new IntersectionObserver((entries ) => {
-  entries.forEach( (entry) => {
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
       entry.target.src = entry.target.getAttribute("data-src");
       entry.target.classList.add("fadeIn");
@@ -123,47 +84,103 @@ images.forEach((image) => {
   observer.observe(image);
 });
 
-
-window.addEventListener('load', () => {
-  const services = document.querySelectorAll('.service');
-  const siteHeader = document.getElementById('siteTitle');
+window.addEventListener("load", () => {
+  const services = document.querySelectorAll(".service");
+  const siteHeader = document.getElementById("siteTitle");
   if (!siteHeader) {
     return;
   }
-  const siteSubtitle = document.getElementById('siteSubtitle');
-  const ctaBtn = document.querySelector('header.header-main .cta-btn');
+  const siteSubtitle = document.getElementById("siteSubtitle");
+  const ctaBtn = document.querySelector("header.header-main .cta-btn");
   siteHeader.style.opacity = 1;
   siteHeader.style.transform = "translateY(0)";
   siteSubtitle.style.opacity = 1;
   siteSubtitle.style.transform = "translateY(0)";
   ctaBtn.style.opacity = 1;
-  
+
   for (let i = 0; i < services.length; i++) {
     setTimeout(() => {
       services[i].style.opacity = 1;
       services[i].style.transform = "translateX(0)";
-    }, 1500 * i)
+    }, 1500 * i);
   }
-})
-  
-
-
-
-
-
-
+});
 
 const yearContainer = document.getElementById("currentYear");
 const currentYear = new Date().getFullYear();
 
-yearContainer.textContent = currentYear + ' ';
+yearContainer.textContent = currentYear + " ";
 yearContainer.style.textDecoration = "none";
 yearContainer.style.display = "inline";
 
-
-document.querySelectorAll('.desktop-nav a').forEach((link) => {
-  if (link.getAttribute('href') === location.pathname) {
+document.querySelectorAll(".desktop-nav a").forEach((link) => {
+  if (link.getAttribute("href") === location.pathname) {
     link.style.fontWeight = 900;
     link.style.color = "indigo";
   }
+});
+
+
+
+// Testimonial slider
+let counter = 0;
+
+function handleDisabledButtons() {
+  const totalSlides = document.querySelectorAll('.swiper-slide').length;
+
+  if (counter >= totalSlides - 1) {
+    document.getElementById('next-testimonial').setAttribute('disabled', true)
+  } else {
+    document.getElementById('next-testimonial').disabled = false;
+  }
+
+  if (counter <= 0) {
+    document.getElementById('prev-testimonial').setAttribute('disabled', true)
+  } else {
+    document.getElementById('prev-testimonial').disabled = false;
+  }
+}
+
+function incrementSlide(element, interval) {
+  counter++
+  const totalSlides = document.querySelectorAll('.swiper-slide').length;
+  counter >= totalSlides - 1? counter = totalSlides - 1 : counter;
+  element.style.transform = `translate3D(${counter * -100}%, 0, 0)`;
+  clearInterval(interval);
+  handleDisabledButtons();
+
+  
+}
+
+function decrementSlide(element, interval) {
+  counter--
+  const totalSlides = document.querySelectorAll('.swiper-slide').length;
+  counter <= 0 ? counter = 0 : counter
+  element.style.transform = `translate3D(${counter * -100}%, 0, 0)`;
+  clearInterval(interval)
+  handleDisabledButtons();
+}
+
+
+function testimonialSlides() {
+  const innerSwiper = document.querySelector(".swiper-wrapper");
+  const slides = document.querySelectorAll(".swiper-slide");
+  const prevBtn = document.getElementById('prev-testimonial')
+  const nextBtn = document.getElementById('next-testimonial')
+  const initSlider = setInterval(() => {
+    counter++
+    innerSwiper.style.transform = `translate3D(${counter * -100}%, 0, 0)`;
+    if (counter >= slides.length - 1) {
+      counter = -1;
+    }
+
+  }, 7000);
+prevBtn.addEventListener('click', () => {
+  decrementSlide( innerSwiper, initSlider);
 })
+nextBtn.addEventListener('click', () => {
+  incrementSlide(innerSwiper, initSlider);
+})
+}
+
+testimonialSlides()
